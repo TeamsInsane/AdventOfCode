f4faa2cb32e4c9de8245016b32d381695b3f3de2
Tilen <kokimedved@gmail.com>
2024-12-04 20:48:33 +0100

    Day two part two

--- src/main/java/com/github/TeamsInsane/challenge/RedNosedReports.java
+++ src/main/java/com/github/TeamsInsane/challenge/RedNosedReports.java
@@ -13,53 +13,90 @@ public class RedNosedReports implements AdventDay {
         int safeCount = 0;
 
         for (String line : readLines("input2.txt")) {
-            List<Integer> arrayOfData = Arrays
+            List<Integer> arrayOfData = new java.util.ArrayList<>(Arrays
                     .stream(line.split(" "))
                     .mapToInt(Integer::parseInt)
                     .boxed()
-                    .toList();
+                    .toList());
 
-            Direction direction = arrayOfData.get(0) > arrayOfData.get(1) ? Direction.DECREASING :
-                    arrayOfData.get(0) < arrayOfData.get(1) ? Direction.INCREASING : Direction.NONE;
 
-            if (direction == Direction.NONE) {
-                continue;
-            }
-
-            boolean safe = true;
-            for (int i = 0; i < arrayOfData.size() - 1; i++) {
-                int firstElement = arrayOfData.get(i);
-                int secondElement = arrayOfData.get(i + 1);
-
-                switch (direction) {
-                    case Direction.DECREASING -> {
-                        if (firstElement < secondElement) {
-                            safe = false;
-                        } else {
-                            safe = checkDifference(firstElement, secondElement);
-                        }
-                    }
-
-                    case Direction.INCREASING -> {
-                        if (secondElement < firstElement) {
-                            safe = false;
-                        } else {
-                            safe = checkDifference(secondElement, firstElement);
-                        }
-                    }
-                }
-
-                if (!safe) {
-                    break;
-                }
-            }
-
-            if (safe) {
+            if (isSafe(arrayOfData)) {
                 safeCount++;
             }
         }
 
-        System.out.println("Part 1 solution: " + safeCount);
+        System.out.println("Part 2 solution: " + safeCount);
+    }
+
+    @PartLoader
+    public void solvePart2() {
+        int safeCount = 0;
+
+        for (String line : readLines("input2.txt")) {
+            List<Integer> arrayOfData = new java.util.ArrayList<>(Arrays
+                    .stream(line.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .boxed()
+                    .toList());
+
+
+            if (isSafe(arrayOfData)) {
+                safeCount++;
+            } else {
+                for (int removeIndex = 0; removeIndex < arrayOfData.size(); removeIndex++) {
+                    int value = arrayOfData.get(removeIndex);
+                    arrayOfData.remove(removeIndex);
+
+                    if (isSafe(arrayOfData)) {
+                        safeCount++;
+                        break;
+                    } else {
+                        arrayOfData.add(removeIndex, value);
+                    }
+                }
+            }
+        }
+
+        System.out.println("Part 2 solution: " + safeCount);
+    }
+
+    public boolean isSafe(List<Integer> arrayOfData) {
+        Direction direction = arrayOfData.get(0) > arrayOfData.get(1) ? Direction.DECREASING :
+            arrayOfData.get(0) < arrayOfData.get(1) ? Direction.INCREASING : Direction.NONE;
+
+        if (direction == Direction.NONE) {
+            return false;
+        }
+
+        boolean safe = true;
+        for (int i = 0; i < arrayOfData.size() - 1; i++) {
+            int firstElement = arrayOfData.get(i);
+            int secondElement = arrayOfData.get(i + 1);
+
+            switch (direction) {
+                case Direction.DECREASING -> {
+                    if (firstElement < secondElement) {
+                        safe = false;
+                    } else {
+                        safe = checkDifference(firstElement, secondElement);
+                    }
+                }
+
+                case Direction.INCREASING -> {
+                    if (secondElement < firstElement) {
+                        safe = false;
+                    } else {
+                        safe = checkDifference(secondElement, firstElement);
+                    }
+                }
+            }
+
+            if (!safe) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     private boolean checkDifference(int firstValue, int secondValue) {
