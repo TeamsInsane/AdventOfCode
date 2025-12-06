aaad5024ad500bc0168a084d51cc404b2ecc3007
Tilen <kokimedved@gmail.com>
2025-12-06 12:27:41 +0100

    Added day1

--- src/main/java/com/github/TeamsInsane/challenge/SecretEntrance.java
+++ src/main/java/com/github/TeamsInsane/challenge/SecretEntrance.java
@@ -6,6 +6,7 @@ import com.github.TeamsInsane.interfaces.AdventDay;
 
 public class SecretEntrance implements AdventDay {
     private final int MAX_VALUE = 99;
+    private final int MIN_VALUE = 0;
     private final int START_VALUE = 50;
 
     @PartLoader
@@ -23,12 +24,48 @@ public class SecretEntrance implements AdventDay {
 
             startValue = Math.floorMod(startValue + value, MAX_VALUE + 1);
 
-            if (startValue == 0) {
+            if (startValue == MIN_VALUE) {
                 solutionCount++;
             }
         }
 
         System.out.println("Solution for part 1: " + solutionCount);
+    }
+
+    @PartLoader
+    public void solvePart2() {
+        int startValue = START_VALUE;
+        int solutionCount = 0;
+
+        for (String lines : readLines("input1.txt")) {
+            char direction = lines.charAt(0);
+            int value = Integer.parseInt(lines.substring(1));
+
+            int addValue = 1;
+
+            if (direction == 'L') {
+                addValue = -addValue;
+            }
+
+            for (int i = 0; i < value; i++) {
+                if (startValue == MAX_VALUE && addValue > 0) {
+                    startValue = MIN_VALUE;
+                } else if (startValue == MIN_VALUE && addValue < 0) {
+                    startValue = MAX_VALUE;
+                } else {
+                    startValue += addValue;
+                }
+
+                if (startValue == MIN_VALUE) {
+                    solutionCount++;
+                }
+            }
+
+
+
+        }
+
+        System.out.println("Solution for part 2: " + solutionCount);
     }
 
     @Override
